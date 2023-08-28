ca81870a3e4929eabe9f361437075e0ec8f349f8
rhyusarmiento <rhyu.sar.15@gmail.com>
2023-08-28 15:34:50 -0600

    fixing/switching database

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("helrld")
+    res.send("test")
 })
 
 app.listen(3001, () => {
-    console.log("running on port 3001")
+    console.log("3001")
 })
